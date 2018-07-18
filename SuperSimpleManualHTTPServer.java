6d35090f9d469e27533265de6f67f71a8f4eeb9c
Steve Geluso <stevegeluso@gmail.com>
2018-07-18 15:42:16 -0700

    Create SuperSimpleManualHTTPServer.java

---
+++ SuperSimpleManualHTTPServer.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.net.*;
+
+class MyHttpServer {
+    public static final int PORT = 6789;
+
+    public static void main(String argv[]) throws Exception {
+        ServerSocket welcomeSocket = new ServerSocket(PORT);
+        System.out.println("Listening on http://localhost:" + PORT);
+
+        boolean isRunning = true;
+        while (isRunning) {
+            System.out.println("waiting for request...");
+            Socket connectionSocket = welcomeSocket.accept();
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            BufferedWriter outToClient = new BufferedWriter(new OutputStreamWriter(connectionSocket.getOutputStream()));
+
+            // peel off the first GET/POST PATH line
+            String requestLine = inFromClient.readLine();
+            System.out.println("REQUEST: " + requestLine);
+
+            // get the next line to collect all the headers
+            String header = inFromClient.readLine();
+            // read lines and assume they're headers until reaching an empty line.
+            while (!header.equals("")) {
+                System.out.println("HEADER: " + header);
+                header = inFromClient.readLine();
+            }
+
+            String message = "<h1>neato</h1>";
+            outToClient.write("HTTP/1.1 200 OK\n");
+            outToClient.write("Content-Length: " + message.length() + "\n");
+            outToClient.write("\n");
+            outToClient.write(message + "\n");
+
+            outToClient.flush();
+            outToClient.close();
+
+            System.out.println("closed request.");
+        }
+    }
+}
